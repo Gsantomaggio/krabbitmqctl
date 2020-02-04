1428bccd3f428f84275cbc5550d076ef1befc282
Gabriele Santomaggio <gsantomaggio@suse.com>
2020-02-04 10:00:40 +0100

    add pod options

--- cmd/cli.go
+++ cmd/cli.go
@@ -17,22 +17,26 @@ type Options struct {
 	kubeConfig string
 	context    string
 	namespace  string
+	podName    string
+
 
 }
 
 var opts = &Options{
 	serviceName: "rabbitmq",
 	namespace: "default",
+	podName: "",
 }
 
 func Run() {
 	cmd := &cobra.Command{}
-	cmd.Use = "Kubernetes rabbitmqctl interface"
-	cmd.Short = "rabbitmqctl interface"
+	cmd.Short = "rabbitmqctl kubernetes interface command "
+	cmd.Use = "rabbitmqctl command"
 	cmd.Flags().StringVarP(&opts.serviceName, "service", "s", opts.serviceName, "RabbitMQ Service")
 	cmd.Flags().StringVar(&opts.context, "context", opts.context, "Kubernetes context to use. Default to current context configured in kubeconfig.")
 	cmd.Flags().StringVar(&opts.kubeConfig, "kubeconfig", opts.kubeConfig, "Path to kubeconfig file to use")
 	cmd.Flags().StringVarP(&opts.namespace, "namespace", "n", opts.namespace, "Kubernetes namespace to use. Default to namespace configured in Kubernetes context")
+	cmd.Flags().StringVarP(&opts.podName, "podname", "p", opts.podName, "Pod where execute the command. Default is \"\" pick one random")
 
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
 
@@ -50,12 +54,12 @@ func Run() {
 
 		defer cancel()
 
-		value, _, err2 := kctl.Run(ctx, config)
+		resultOk, _, err2 := kctl.Run(ctx, config)
 		if err2 != nil {
 			fmt.Println(err2)
 			os.Exit(1)
 		}
-		log.Printf("value %s", value)
+		fmt.Printf("%s", resultOk)
 
 		return nil
 	}
@@ -69,34 +73,29 @@ func parseConfig(args []string) (*kctl.Config, error) {
 		return nil, err
 	}
 
-
 	return &kctl.Config{
 		KubeConfig:  kubeConfig,
 		ContextName: opts.context,
 		NameSpace:   opts.namespace,
 		CtlCommand: args,
 		ServiceName: opts.serviceName,
+		PodName: opts.podName,
 	}, nil
 }
 
 func getKubeConfig() (string, error) {
 	var kubeconfig string
-
 	if kubeconfig = opts.kubeConfig; kubeconfig != "" {
 		return kubeconfig, nil
 	}
-
 	if kubeconfig = os.Getenv("KUBECONFIG"); kubeconfig != "" {
 		return kubeconfig, nil
 	}
-
-	// kubernetes requires an absolute path
 	home, err := homedir.Dir()
 	if err != nil {
 		return "", errors.Wrap(err, "failed to get user home directory")
 	}
 
 	kubeconfig = filepath.Join(home, ".kube/config")
-
 	return kubeconfig, nil
 }
--- kctl/config.go
+++ kctl/config.go
@@ -6,4 +6,5 @@ type Config struct {
 	NameSpace   string
 	ServiceName string
 	CtlCommand  []string
+	PodName string
 }
